ed73e4bc02a04dbd7dbf3d6847819ab1822eace5
Sai Aik Seng Hein Seng <nangmwephowneseng@JSHS-2.local>
2025-12-27 20:50:39 +0700

    mt

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./register.jsx";
 import SubmitData from "./SubmitData.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         
+
         <Route path="/register" element={<Register />} />
         <Route path="/submit-data" element={<SubmitData />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
